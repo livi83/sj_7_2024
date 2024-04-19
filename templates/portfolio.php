1f51733930b6999e1ff4871d53a7d3d80997c153
Lívia Kelebercová <livia.kelebercova@gmail.com>
2024-04-19 12:28:49 +0200

    fix

--- templates/portfolio.php
+++ templates/portfolio.php
@@ -1,36 +1,31 @@
 <?php
 include('partials/header.php');
 ?> 
-        <main>
-             <?php include('partials/banner.php');?>
-              <section class="container">
-                <?php
-                    //generate_portfolio(2,4);
-                    $portfolio_object = new Portfolio();
-                    $portfolio = $portfolio_object->select();
-                    
-                    for ($i=0;$i<count($portfolio);$i++){
-                      $temp_i = $i+1;
-                      if($temp_i%4==1){
-                          echo '<div class="row">';
-                          echo '<div class="col-25 portfolio text-white text-center" style = "background-image: url(\''.$portfolio[$i]->image.'\');"'.'>';
-                          //echo '<a href="">'.$portfolio[$i]->name.'</a>';
-                          echo '<a href="../templates/portfolio-single.php?id='.$portfolio[$i]->id.'">'.$portfolio[$i]->name.'</a>';
-                          echo '</div>';
+<main>
+    <?php include('partials/banner.php');?>
+    <section class="container">
+        <?php
+        //generate_portfolio(2,4);
+        $portfolio_object = new Portfolio();
+        $portfolio = $portfolio_object->select();
+        
+        for ($i = 0; $i < count($portfolio); $i++) {
+            $temp_i = $i + 1;
+            if ($temp_i % 4 == 1) {
+                echo '<div class="row">';
+            }
 
-                      }else{
-                        echo '<div class="col-25 portfolio text-white textcenter" style = "background-image: url(\''.$portfolio[$i]->image.'\');"'.'>';
-                        //echo '<a href="">'.$portfolio[$i]->name.'</a>';
-                        echo '<a href="../templates/portfolio-single.php?id='.$portfolio[$i]->id.'">'.$portfolio[$i]->name.'</a>';
-                        echo '</div>';
-                        echo'</div>';
-                      }
-                  }
-                ?>
-               
-            </section>   
+            echo '<div class="col-25 portfolio text-white text-center" style="background-image: url(\''.$portfolio[$i]->image.'\');">';
+            echo '<a href="">'.$portfolio[$i]->name.'</a>';
+            echo '</div>';
 
-        </main>
+            if ($temp_i % 4 == 0 || $temp_i == count($portfolio)) {
+                echo '</div>'; // Close row after every four items or at the end
+            }
+        }
+        ?>
+    </section>   
+</main>
 <?php
-  include_once('partials/footer.php')
+include_once('partials/footer.php')
 ?> 
